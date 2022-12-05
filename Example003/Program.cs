b0b652982e9dae8e65d85c697d30fb0bb7650826
Karina_Belxhenko <karin.bel4enko@yandex.ru>
2022-12-05 14:47:45 +0300

    для Д/З/

---
+++ Example003/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+int a = 2;
+int b = 3;
+int c = 7;
+int max = false;
+if(a>max)max = a;
+if(b>max)max = b;
+if(c>max)max = c;
+Console.Write("max = ";)
+Console.WriteLine("Hello, World!");
