84a202738cc13df305268e1574cebcb6256e9d72
Karina_Belxhenko <karin.bel4enko@yandex.ru>
2022-12-05 14:26:41 +0300

    для Д/З/

--- Example001/Program.cs
+++ Example001/Program.cs
@@ -1,1 +1,9 @@
 ﻿Console.WriteLine("2Hello, World!");
+// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+int a = -9; 
+int b = -3;
+int max = a;
+if(a>max)max = a;
+if(b>max)max = b;
+Console.Write("max = ");
+Console.WriteLine(max);
